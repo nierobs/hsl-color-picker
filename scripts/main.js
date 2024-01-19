fea37c2ea6e288a540c9f67965168e9e71cce456
Robson Niemeyer <nierobs@hotmail.com>
2024-01-19 18:18:52 -0300

    Store color in the browser session

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const preview = document.getElementById('preview')
+
 const hsl = [
     {
         input: document.getElementById('h-input'),
@@ -15,12 +17,41 @@ const hsl = [
     }
 ]
 
-const preview = document.getElementById('preview')
+const storage = {
+    inputValue(key) {
+        return document.getElementById(key).value
+    },
+    get(key) {
+        return localStorage.getItem(key)
+    },
+    set(key) {
+        return localStorage.setItem(key, this.inputValue(key))
+    },
+}
+
+const updateColor = (span, value) => {
+        span.textContent = value
+        preview.style.backgroundColor = `hsl(${hsl[0].input.value}, ${hsl[1].input.value}%, ${hsl[2].input.value}%)`
+}
 
 hsl.forEach((obj) => {
     const { input, span } = obj
+
     input.addEventListener('input', () => {
-        span.textContent = input.value
-        preview.style.backgroundColor = `hsl(${hsl[0].input.value}, ${hsl[1].input.value}%, ${hsl[2].input.value}%)`
+        updateColor(span, input.value)
+
+        // Save color data in the browser session
+        storage.set(input.id)
+    })
+})
+
+// Read color data in the browser session and update DOM on page load
+window.addEventListener('load', () => {
+    hsl.forEach((obj) => {
+        const { input, span } = obj
+        const value = storage.get(input.id)
+
+        input.value = value
+        updateColor(span, value)
     })
 })
