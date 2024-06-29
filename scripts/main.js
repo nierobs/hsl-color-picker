9c40f7701424871d16b7309762e625b4f67cd8c5
Robson Niemeyer <nierobs@hotmail.com>
2024-06-29 18:31:14 -0300

    Add random color

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,45 @@
 'use strict'
 
-const color = document.getElementById('color')
-const caption = document.getElementById('caption')
-const inputs = document.getElementsByTagName('input')
+const color = document.getElementById('color');
+const caption = document.getElementById('caption');
+const inputs = document.getElementsByTagName('input'); 
 
 const updateDOM = () => {
-    const hsl = `hsl(${inputs[0].value}, ${inputs[1].value}%, ${inputs[2].value}%)`
+    const hsl = `hsl(${inputs[0].value}, ${inputs[1].value}%, ${inputs[2].value}%)`;
 
-    color.style.backgroundColor = hsl
-    caption.textContent = hsl
+    color.style.backgroundColor = hsl;
+    caption.textContent = hsl;
+};
+
+const storeValue = (input) => {
+    localStorage.setItem(input.id, input.value);
 }
 
-for (let i = 0; i < inputs.length; i++) {
-    const { id } = inputs[i]
-    const savedValue = localStorage.getItem(id)
+const getRandomH = () => Math.floor(Math.random() * 24) * 15;
+const getRandomSL = () => Math.floor(Math.random() * 24) * 4 + 4;
 
-    if (savedValue !== null) inputs[i].value = savedValue
+const randomColor = () => {
+    inputs[0].value = getRandomH();
+    inputs[1].value = getRandomSL();
+    inputs[2].value = getRandomSL();
 
-    inputs[i].addEventListener('input', () => {
-        updateDOM()
-        localStorage.setItem(id, inputs[i].value)
-    })
-}
+    updateDOM();
 
-updateDOM()
+    for (let i = 0; i < inputs.length; i++) {
+        storeValue(inputs[i]);
+    }
+};
+
+window.addEventListener('load', () => {
+    for (let i = 0; i < inputs.length; i++) {
+        const item = localStorage.getItem(inputs[i].id);
+
+        if (item !== null) inputs[i].value = item;
+
+        inputs[i].addEventListener('input', () => storeValue(inputs[i]));
+    }
+
+    document.getElementById('random').addEventListener('click', () => randomColor());
+
+    updateDOM();
+});
